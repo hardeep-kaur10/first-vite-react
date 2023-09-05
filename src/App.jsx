0ac8d2edc3e729c27f803cd2d5020870737000f5
Hardeep Kaur <deepkaur200608@gmail.com>
2023-09-06 01:10:15 +0530

    intro to props

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,44 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
+// const Mybutton = (props)=>{
+//   console.log(props.title);
 //   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
+//     <div> 
+//     <h2>{props.title}</h2>
+//     <button>hello</button>
+//     </div>
+
+//   );
+// }
+// function App() {
+
+//   return ( 
+//   <div>
+//     <Mybutton title = "send" />
+//   </div>
 //   )
 // }
 
+const Mybutton = (parameters)=>{
+console.log(parameters.title);
+  return (
+    <div> 
+    <h5>{parameters.title}</h5>
+    <button>{parameters.text}</button>
+    </div>
 
-
+  );
+}
 function App() {
 
-  return ( <h1>  Hello world </h1>)
+  return ( 
+  <div>
+    <Mybutton title="1" text="use me"/>
+    <Mybutton title="2" text="use"/>
+  </div>
+  )
 }
 
+
 export default App
+
+
+// export default App
